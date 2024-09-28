bb8391c53bce7a4130e1111f1435684cd6f9d8c9
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-09-29 00:55:58 +0800

    "minimal shop"

--- js/test.js
+++ js/test.js
@@ -21,4 +21,20 @@ function changeImage(imageSrc) {
 			card.style.boxShadow = 'none';
 	});
 }
+// modal shop
+	function changeImage1(imageSrc) {
+			document.getElementById('mainImage1').src = imageSrc;
+	}
 
+	function changeImage2(imageSrc) {
+			document.getElementById('mainImage2').src = imageSrc;
+	}
+	function changeImage3(imageSrc) {
+		document.getElementById('mainImage3').src = imageSrc;
+	}
+	function changeImage4(imageSrc) {
+		document.getElementById('mainImage4').src = imageSrc;
+	}
+	function changeImage5(imageSrc) {
+		document.getElementById('mainImage5').src = imageSrc;
+	}
